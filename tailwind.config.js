668d0a7b8ca5fa012b5f691b754ea6ea1cb94d64
kira <kira@mail.ru>
2026-04-08 13:40:38 +0300

    Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+        screens: {
+            'xs': '320px',
+            'sm': '576px',
+            'md': '768px',
+            'lg': '1024px',
+            'xl': '1366px',
+            '2xl': '1600px',
+            '3xl': '1920px',
+        },
+        extend: {},
+    },
+    plugins: [],
+}
